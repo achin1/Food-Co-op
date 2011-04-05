e44280f52d93bf5fade2337455c298b50b76e485
Andrew Marshall <andrew@johnandrewmarshall.com>
2011-04-05 16:59:19 -0400

    Proper Java variable names

--- src/Member.java
+++ src/Member.java
@@ -6,12 +6,12 @@ public class Member extends Model {
 	private String firstName;
 	private String lastName;
 	private String email;
-	private Date last_signup_date;
-	private int membership_length;
-	private int membership_type;
-	private int year_in_school;
-	private boolean recieve_email;
-	private boolean is_active;
+	private Date lastSignupDate;
+	private int membershipLength;
+	private int membershipType;
+	private int yearInSchool;
+	private boolean receiveEmail;
+	private boolean isActive;
 	
 	public Member() {}
 	
@@ -23,12 +23,12 @@ public class Member extends Model {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.email = email;
-		this.last_signup_date = last_signup_date;
-		this.membership_length = membership_length;
-		this.membership_type = membership_type;
-		this.year_in_school = year_in_school;
-		this.recieve_email = recieve_email;
-		this.is_active = is_active;
+		this.lastSignupDate = last_signup_date;
+		this.membershipLength = membership_length;
+		this.membershipType = membership_type;
+		this.yearInSchool = year_in_school;
+		this.receiveEmail = recieve_email;
+		this.isActive = is_active;
 	}
 	
 	public static ArrayList<Member> findByName(String firstName, String lastName) {
